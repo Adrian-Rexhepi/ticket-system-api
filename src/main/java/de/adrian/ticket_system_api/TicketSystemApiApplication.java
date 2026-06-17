d76832b2ca0db05a4e855f0a802b63a6ba8b0c2b
Adrian <adrimail916@gmail.com>
2026-06-17 20:27:45 +0200

    Initial Commit

---
+++ src/main/java/de/adrian/ticket_system_api/TicketSystemApiApplication.java
@@ -0,0 +1,13 @@
+package de.adrian.ticket_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSystemApiApplication.class, args);
+	}
+
+}
