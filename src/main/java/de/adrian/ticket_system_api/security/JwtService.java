b7760818518741165602002a4e8404599ed5706e
Adrian <adrimail916@gmail.com>
2026-06-23 21:14:51 +0200

    Added exception handling into JwtService

--- src/main/java/de/adrian/ticket_system_api/security/JwtService.java
+++ src/main/java/de/adrian/ticket_system_api/security/JwtService.java
@@ -3,9 +3,13 @@ package de.adrian.ticket_system_api.security;
 import java.util.Date;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import de.adrian.ticket_system_api.entity.User;
 
 public class JwtService {
+
+    private static final String SECRET = System.getenv("JWT_SECRET");
+    private static final long EXPIRATION_TIME = 3600000;
 
     public String generateToken(User user) {
         
@@ -13,29 +17,41 @@ public class JwtService {
                 .withSubject(user.getUsername())
                 .withClaim("role", "USER")
                 .withIssuedAt(new Date())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 3600000))
-                .sign(Algorithm.HMAC256("secret"));
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(Algorithm.HMAC256(SECRET));
 
         return token;
     }
 
     public String extractUsername(String token) {
-        return JWT.require(Algorithm.HMAC256("secret"))
-                .build()
-                .verify(token)
-                .getSubject();
+        try {
+            return JWT.require(Algorithm.HMAC256(SECRET))
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return null;
+        }
     }
 
     public boolean isTokenValid(String token, User user) {
-        String username = extractUsername(token);
-        return (username.equals(user.getUsername()) && !isTokenExpired(token));
+        try {
+            String username = extractUsername(token);
+            return (username.equals(user.getUsername()) && !isTokenExpired(token));
+        } catch (JWTVerificationException e) {
+            return false;
+        }
     }
 
     private boolean isTokenExpired(String token) {
-        Date expiration = JWT.require(Algorithm.HMAC256("secret"))
-                .build()
-                .verify(token)
-                .getExpiresAt();
-        return expiration.before(new Date());
+        try {
+            Date expiration = JWT.require(Algorithm.HMAC256(SECRET))
+                    .build()
+                    .verify(token)
+                    .getExpiresAt();
+            return expiration.before(new Date());
+        } catch (JWTVerificationException e) {
+            return true;
+        }
     }
 }
